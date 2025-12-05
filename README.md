aac03afb45a9e20d995c6c82fdcaa44294f2134c
kang xuan <73049816+lowkangxuan@users.noreply.github.com>
2025-12-05 17:58:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Note to self
+> Run `npm install` upon creating a new repository with this template to install all dependencies
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
